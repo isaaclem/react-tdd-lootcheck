d0c5b7c46ab9d49270d76c70b153b5035dac6301
isaaclem <lem.chiew.choon@gmail.com>
2019-04-23 15:56:54 +0800

    Lesson 26: TDD with local state

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -2,10 +2,21 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 export class Wallet extends Component {
+  state = { balance: 0 };
+
+  updateBalance = event => {
+    this.setState({ balance: parseInt(event.target.value, 10) });
+  }
+
   render() {
     return (
       <div>
         <h3 className='balance'>Wallet Balance: {this.props.balance}</h3>
+        <br />
+        <input 
+          className='input-wallet' 
+          onChange={this.updateBalance} 
+        />
       </div>
     )
   }
--- src/components/Wallet.test.js
+++ src/components/Wallet.test.js
@@ -12,6 +12,23 @@ describe('Wallet', () => {
 
   it('display the balance from props', () => {
     expect(wallet.find('.balance').text()).toEqual('Wallet Balance: 20')
-  })
+  });
+
+  it('creates an input to deposit into or withdraw from the balance', () => {
+    expect(wallet.find('.input-wallet').exists()).toBe(true);
+  });
+
+  describe('When the user types into the wallet input', () => {
+    const userBalance = '25'; 
+
+    beforeEach(() => {
+      wallet.find('.input-wallet')
+        .simulate('change', { target: { value: userBalance }});
+    });
+
+    it('updates a local balance in `state` and converts it to a number', () => {
+      expect(wallet.state().balance).toEqual(parseInt(userBalance, 10))
+    });
+  });
 
 });
