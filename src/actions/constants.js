79659bbaf6d4897ab71652a9af8ecb2a9872bf20
isaaclem <lem.chiew.choon@gmail.com>
2019-04-23 21:04:27 +0800

    Lesson 31: TDD with redux-thunk + API calling

--- src/actions/constants.js
+++ src/actions/constants.js
@@ -1,3 +1,4 @@
 export const SET_BALANCE = 'set_balance';
 export const DEPOSIT_BALANCE = 'deposit_balance';
 export const WITHDRAW_BALANCE = 'withdraw_balance';
+export const FETCH_BITCOIN = 'fetch_bitcoin';
